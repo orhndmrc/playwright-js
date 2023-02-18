4284caeb574d01ce1ac0fb22e1d5d92b074ecee8
orhndmrc <60530416+orhndmrc@users.noreply.github.com>
2023-02-18 13:30:37 -0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,14 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['junit', { outputFile: 'results.xml' }], ['json', { outputFile: 'results.json' }], ['list', { printSteps: true, }]],
+  reporter: [['html'], ['junit', { outputFile: 'results.xml' }], ['json', { outputFile: 'results.json' }], ['list', { printSteps: true, }],  [
+    "allure-playwright",
+    {
+      detail: true,
+      outputFolder: "allure-results",
+      suiteTitle: false,
+    },
+  ]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
